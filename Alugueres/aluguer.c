de41f0cc1ad11cf53a9a0209839f29fbbfe7167c
a18851 <98275523+a18851@users.noreply.github.com>
2023-05-21 23:32:39 +0100

    update aluguer com getdata

--- Alugueres/aluguer.c
+++ Alugueres/aluguer.c
@@ -1,17 +1,36 @@
 /**
  * @file aluguer.c
- * @author your name (you@domain.com)
+ * @author lugon (a18851@alunos.ipca.pt)
  * @brief 
  * @version 0.1
- * @date 2023-03-23
+ * @date 2023-05-21
  * 
  * @copyright Copyright (c) 2023
  * 
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h> 
+#include <time.h>
+#include <math.h>
+#include "aluguer.h"
+
+Data getDate() {
+    Data dataAtual;
+
+    // Obtém o timestamp atual
+    time_t timestamp = time(NULL);
+
+    // Converte o timestamp para uma estrutura tm local
+    struct tm *local_time = localtime(&timestamp);
+
+    // Preenche os componentes da data atual
+    dataAtual.ano = local_time->tm_year + 1900;
+    dataAtual.mes = local_time->tm_mon + 1;
+    dataAtual.dia = local_time->tm_mday;
+
+    return dataAtual;
+}
 
--- Alugueres/aluguer.h
+++ Alugueres/aluguer.h
@@ -1,27 +1,29 @@
+
 #ifndef ALUGUER_H
 #define ALUGUER_H
-#define SIZE 50
+#define TAMANHO 50
 
-
-typedef struct data{
+typedef struct Data{
     int ano;
     short mes;
     short dia;
 } Data;
 
 
-typedef struct aluguer
-{
-int idCliente;
-int idMeio; 
-Data data;
-struct aluguer * next;
-
+typedef struct Aluguer {
+    int idCliente;
+    int idMeio;
+    Data data;
+    struct Aluguer *proximo;
+    struct Aluguer *anterior;
 } Aluguer;
 
+typedef struct Fila {
+    Aluguer *inicio;
+    Aluguer *fim;
+} Fila;
 
-bool novoAluguer(Aluguer ** alugueres, Meio * meios, int idCliente, int idMeio, int ano, short mes, short dia);
-void listarAlugueres(Aluguer *alugueres);
+Data getDate();
 
 
 #endif
